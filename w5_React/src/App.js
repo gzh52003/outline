8facfe24c14d3dd4c7cffe7c606eda43575597a0
aaron <jeroo@qq.com>
2020-09-07 16:01:58 +0800

    day6-1 配置基于React的环境、

---
+++ w5_React/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Home from './pages/Home.jsx'
+
+function App(){
+    return <div>
+        App, laoxie 666
+
+        <Home/>
+    </div>
+}
+
+export default App;
---
+++ w5_React/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 返回一个commonJS模块
+module.exports = {
+    // 入口：webpack从这里开始分析和编译文件
+    entry:path.join(__dirname,'./src/main.js'),
+
+    // 本地服务器：webpack-dev-server
+    devServer:{
+        // 设置服务器根目录（网站根目录）
+        contentBase:path.join(__dirname,'./public'),
+        // port:3000
+    },
+
+    // 加载器loader
+    // 每一类文件应该有一个加载器，用于解析这类文件
+    module:{
+        rules:[
+            // 配置js加载器（ babel-loader），解析js规则
+            {
+                test:/\.jsx?$/,
+                use:[{
+                    loader:'babel-loader',
+
+                    // babel-loader的参数
+                    options:{
+                        presets:['@babel/preset-react']
+                    }
+                }]
+            }
+        ]
+    },
+
+    // webpack插件
+    plugins:[
+        // HtmlWebpackPlugin插件作用：生成一个index.html文件
+        new HtmlWebpackPlugin({
+            template:path.join(__dirname,'./public/index.html')
+        })
+    ]
+}
